7a88cdaf1e2fb9557db5973f0468052a3c1684db
Björn Kübli <bjoern.kuebli@protonmail.com>
2025-12-17 19:17:25 +0100

    chore: fix image src generation, rebuild docs

--- src/generate_page.py
+++ src/generate_page.py
@@ -17,7 +17,7 @@ def generate_page(from_path: str, template_path: str, dest_path: str, basepath: 
             template_html = template_html.replace("{{ Title }}", title)
             template_html = template_html.replace("{{ Content }}", html_node.to_html())
             template_html = template_html.replace('href="/', f'href="{basepath}')
-            template_html = template_html.replace('url="/', f'url="{basepath}')
+            template_html = template_html.replace('src="/', f'src="{basepath}')
 
             with open(dest_path, "x") as f_html:
                 f_html.write(template_html)
